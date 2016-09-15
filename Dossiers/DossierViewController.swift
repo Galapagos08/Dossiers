b742e07b0bb3ffbfd9275f14b2880886c22af27d
Dan Esrey <danesrey@dans-macbook-air.local>
2016-09-15 15:54:59 -0400

    passing data to DossierVC

--- Dossiers/DossierViewController.swift
+++ Dossiers/DossierViewController.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class DossierViewController: UIViewController {
     
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var locationField: UITextField!
+    @IBOutlet var associatesField: UITextField!
+    @IBOutlet var occupationField: UITextField!
+    @IBOutlet var languagesField: UITextField!
+    var agent: Dossier?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        title = agent?.name
+        self.nameField.text = agent?.name
+        self.locationField.text = agent?.lastKnownLocation
+        self.associatesField.text = ""//agent?.knownAssociates
+        self.occupationField.text = agent?.occupation
+        self.languagesField.text = ""//agent?.languagesSpoken
+     }
     
     
 }
--- Dossiers/OrganizationViewController.swift
+++ Dossiers/OrganizationViewController.swift
@@ -9,12 +9,14 @@
 import UIKit
 
 class OrganizationViewController: UIViewController {
- 
-    @IBOutlet var organizationLabel: UILabel!
+    
     @IBOutlet var button3: UIButton!
     @IBOutlet var button2: UIButton!
     @IBOutlet var button1: UIButton!
     var organization: Organization?
+    var agent: Dossier?
+    var window: UIWindow?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +30,22 @@ class OrganizationViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if button1 .isEqual(sender) {
+            agent = (organization?.operatives)![0]
+            if let destination = segue.destination as? DossierViewController {
+                destination.agent = self.agent
+            }
+        } else if button2 .isEqual(sender) {
+            agent = (organization?.operatives)![1]
+            if let destination = segue.destination as? DossierViewController {
+                destination.agent = self.agent
+            }
+        } else if button3 .isEqual(sender) {
+            agent = (organization?.operatives)![2]
+            if let destination = segue.destination as? DossierViewController {
+                destination.agent = self.agent
+            }
+        }
+    }
 }
-
